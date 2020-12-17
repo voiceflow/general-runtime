11575c638762d57d39e695c34aec3d8abe2d2807
Tyler Han <tyler@voiceflow.com>
2020-12-16 19:15:51 -0500

    chore: fix all tests

--- tests/lib/services/runtime/index.unit.ts
+++ tests/lib/services/runtime/index.unit.ts
@@ -24,19 +24,6 @@ describe('runtime manager unit tests', () => {
       const trace = { foo1: 'bar1' };
 
       const runtime = {
-        setEvent: sinon.stub(),
-        turn: {
-          get: sinon.stub().returns(false), // TurnType.END false
-          set: sinon.stub(),
-        },
-        storage: {
-          set: sinon.stub(),
-          get: sinon.stub().returns(null), // no stream
-        },
-        stack: {
-          isEmpty: sinon.stub().returns(false), // stack no empty
-        },
-        variables: { set: sinon.stub() },
         update: sinon.stub(),
         getRawState: sinon.stub().returns(rawState),
         trace: { get: sinon.stub().returns(trace), addTrace: sinon.stub() },
@@ -65,27 +52,7 @@ describe('runtime manager unit tests', () => {
       const request = { foo3: 'bar3' };
       const context = { state, request, versionID: VERSION_ID } as any;
       expect(await runtimeManager.handle(context)).to.eql({ state: rawState, trace, request, versionID: VERSION_ID });
-      expect(client.createRuntime.args).to.eql([
-        [
-          VERSION_ID,
-          state,
-          request,
-          {
-            api: { getProgram: services.dataAPI.getProgram },
-            handlers: 'foo',
-          },
-        ],
-      ]);
-      expect(runtime.setEvent.args[0][0]).to.eql(EventType.handlerWillHandle);
-      const fn = runtime.setEvent.args[0][1];
-      const event = { runtime: { foo4: 'bar3' }, node: { id: 'node-id' } };
-      fn(event);
-      expect(runtime.trace.addTrace.args).to.eql([[{ type: 'block', payload: { blockID: event.node.id } }]]);
-      expect(runtime.turn.set.args).to.eql([
-        [TurnType.REQUEST, request],
-        [TurnType.PREVIOUS_OUTPUT, null],
-      ]);
-      expect(runtime.variables.set.args).to.eql([[Variables.TIMESTAMP, Math.floor(clock.now / 1000)]]);
+      expect(client.createRuntime.args).to.eql([[VERSION_ID, state, request]]);
       expect(runtime.update.callCount).to.eql(1);
     });
 
@@ -94,19 +61,6 @@ describe('runtime manager unit tests', () => {
       const trace = { foo1: 'bar1' };
 
       const runtime = {
-        setEvent: sinon.stub(),
-        turn: {
-          set: sinon.stub(),
-          get: sinon.stub(),
-        },
-        storage: {
-          set: sinon.stub(),
-          get: sinon.stub().returns({ action: 'random' }), // stream
-        },
-        stack: {
-          isEmpty: sinon.stub().returns(true), // stack is empty
-        },
-        variables: { set: sinon.stub() },
         update: sinon.stub(),
         getRawState: sinon.stub().returns(rawState),
         trace: { get: sinon.stub().returns(trace), addTrace: sinon.stub() },
@@ -134,7 +88,6 @@ describe('runtime manager unit tests', () => {
       const context = { state: {}, request: {}, versionID: VERSION_ID } as any;
       expect(await runtimeManager.handle(context)).to.eql({ state: rawState, trace, request: {}, versionID: VERSION_ID });
       expect(utils.Handlers.callCount).to.eql(1);
-      expect(runtime.trace.addTrace.args[0]).to.eql([{ type: 'end' }]);
     });
   });
 });
