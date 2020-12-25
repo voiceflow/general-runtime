b41e3e4e1fd5c9715010e6946df0b3006b271524
Tyler Han <tyler@voiceflow.com>
2020-12-25 15:44:53 -0500

    chore: get intent request from api

--- lib/services/nlu/index.ts
+++ lib/services/nlu/index.ts
@@ -1,4 +1,3 @@
-import { Prediction } from '@azure/cognitiveservices-luis-runtime/src/models/index';
 import { IntentRequest, RequestType } from '@voiceflow/general-types';
 
 import { Context, ContextHandler } from '@/types';
@@ -11,7 +10,7 @@ export const utils = {};
 class NLU extends AbstractManager<{ utils: typeof utils }> implements ContextHandler {
   // TODO: implement NLU handler
   handle = async (context: Context) => {
-    if (typeof context.request.payload !== 'string') {
+    if (context.request.type !== RequestType.TEXT) {
       return context;
     }
 
@@ -22,21 +21,11 @@ class NLU extends AbstractManager<{ utils: typeof utils }> implements ContextHan
     }
 
     try {
-      const { data } = await this.services.axios.post<{ query: string } & Pick<Prediction, 'intents' | 'topIntent' | 'entities'>>(
-        `${this.config.GENERAL_SERVICE_ENDPOINT}/runtime/${version.projectID}/predict`,
-        { query: context.request.payload }
-      );
+      const { data } = await this.services.axios.post<IntentRequest>(`${this.config.GENERAL_SERVICE_ENDPOINT}/runtime/${version.projectID}/predict`, {
+        query: context.request.payload,
+      });
 
-      const intentRequest: IntentRequest = {
-        type: RequestType.INTENT,
-        payload: {
-          query: context.request.payload,
-          slots: Object.values(data.entities), // TODO: figure out LUIS entities typ and add adapters
-          intent: { name: data.topIntent },
-        },
-      };
-
-      return { ...context, request: intentRequest };
+      return { ...context, request: data };
     } catch (err) {
       // eslint-disable-next-line no-console
       console.error(err);
