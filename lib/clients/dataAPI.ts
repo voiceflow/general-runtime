0167cefb5e580795c370b6b5a56860c2935a24d7
Tyler Han <tyler@voiceflow.com>
2021-01-20 22:21:18 -0500

    chore: write tests

--- lib/clients/dataAPI.ts
+++ lib/clients/dataAPI.ts
@@ -8,17 +8,17 @@ import Static from './static';
 /**
  * Build all clients
  */
-export default (config: Config) => {
+export default (config: Config, API = { LocalDataApi, RemoteDataAPI, CreatorDataApi }) => {
   let dataAPI: DataAPI<any, any>;
   if (config.PROJECT_SOURCE) {
-    dataAPI = new LocalDataApi({ projectSource: config.PROJECT_SOURCE }, { fs: Static.fs, path: Static.path });
+    dataAPI = new API.LocalDataApi({ projectSource: config.PROJECT_SOURCE }, { fs: Static.fs, path: Static.path });
   } else if (config.ADMIN_SERVER_DATA_API_TOKEN && config.VF_DATA_ENDPOINT) {
-    dataAPI = new RemoteDataAPI(
+    dataAPI = new API.RemoteDataAPI(
       { platform: 'general', adminToken: config.ADMIN_SERVER_DATA_API_TOKEN, dataEndpoint: config.VF_DATA_ENDPOINT },
       { axios: Static.axios }
     );
   } else if (config.CREATOR_API_AUTHORIZATION && config.CREATOR_API_ENDPOINT) {
-    dataAPI = new CreatorDataApi({ endpoint: `${config.CREATOR_API_ENDPOINT}/v2`, authorization: config.CREATOR_API_AUTHORIZATION });
+    dataAPI = new API.CreatorDataApi({ endpoint: `${config.CREATOR_API_ENDPOINT}/v2`, authorization: config.CREATOR_API_AUTHORIZATION });
   } else {
     throw new Error('no data API env configuration set');
   }
