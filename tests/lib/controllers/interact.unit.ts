269b422f78f8de4ca7e648e71f08a6f5dc214034
Justin Goping <jgoping@gmail.com>
2021-02-02 11:44:21 -0500

    test: add unit test for tts handler omission

--- tests/lib/controllers/interact.unit.ts
+++ tests/lib/controllers/interact.unit.ts
@@ -38,5 +38,39 @@ describe('interact controller unit tests', () => {
       expect(services.chips.handle.args).to.eql([[output('tts')]]);
       expect(services.metrics.generalRequest.callCount).to.eql(1);
     });
+
+    it('omits TTS if specified in config', async () => {
+      const req = {
+        body: { state: { foo: 'bar' }, request: 'request', config: { tts: false } },
+        params: { versionID: 'versionID' },
+        query: { locale: 'locale' },
+      };
+      const context = { state: req.body.state, request: req.body.request, versionID: req.params.versionID, data: { locale: req.query.locale } };
+      const output = (data: string) => ({ ...context, data, end: false });
+
+      const services = {
+        state: { handle: sinon.stub().resolves(output('state')) },
+        asr: { handle: sinon.stub().resolves(output('asr')) },
+        nlu: { handle: sinon.stub().resolves(output('nlu')) },
+        tts: { handle: sinon.stub().resolves(output('tts')) },
+        chips: { handle: sinon.stub().resolves(output('chips')) },
+        runtime: { handle: sinon.stub().resolves(output('runtime')) },
+        dialog: { handle: sinon.stub().resolves(output('dialog')) },
+        metrics: { generalRequest: sinon.stub() },
+      };
+
+      const interactController = new Interact(services as any, null as any);
+
+      expect(await interactController.handler(req as any)).to.eql(output('chips'));
+
+      expect(services.state.handle.args).to.eql([[context]]);
+      expect(services.asr.handle.args).to.eql([[output('state')]]);
+      expect(services.nlu.handle.args).to.eql([[output('asr')]]);
+      expect(services.dialog.handle.args).to.eql([[output('nlu')]]);
+      expect(services.runtime.handle.args).to.eql([[output('dialog')]]);
+      expect(services.tts.handle.callCount).to.eql(0);
+      expect(services.chips.handle.args).to.eql([[output('runtime')]]);
+      expect(services.metrics.generalRequest.callCount).to.eql(1);
+    });
   });
 });
