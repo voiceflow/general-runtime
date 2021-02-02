efedd8556075a8de70674a8cfb699a75335a2794
Evgeny <overhawlin@gmail.com>
2021-02-02 12:23:15 +0300

    fix: trace

--- lib/services/runtime/handlers/visual.ts
+++ lib/services/runtime/handlers/visual.ts
@@ -1,12 +1,13 @@
 import { NodeType, TraceType } from '@voiceflow/general-types';
-import { Node, TraceFrame } from '@voiceflow/general-types/build/nodes/visual';
+import { Node } from '@voiceflow/general-types/build/nodes/visual';
 import { HandlerFactory } from '@voiceflow/runtime';
 
 const VisualHandler: HandlerFactory<Node> = () => ({
   canHandle: (node) => node.type === NodeType.VISUAL,
   handle: (node, runtime) => {
     runtime.trace.debug('__visual__ - entered');
-    runtime.trace.addTrace<TraceFrame>({
+
+    runtime.trace.addTrace({
       type: TraceType.VISUAL,
       payload: node.data,
     });
