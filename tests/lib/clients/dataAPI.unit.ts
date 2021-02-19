b250c2837a6d06ad3ea22820b30081af5971007b
Justin Goping <jgoping@gmail.com>
2021-02-19 15:09:15 -0500

    test: update unit tests

--- tests/lib/clients/dataAPI.unit.ts
+++ tests/lib/clients/dataAPI.unit.ts
@@ -10,9 +10,10 @@ describe('dataAPI client unit tests', () => {
     sinon.restore();
   });
 
-  it('local api', () => {
+  it('local api', async () => {
+    const initStub = sinon.stub();
     const API = {
-      LocalDataApi: sinon.stub().returns({ type: 'local' }),
+      LocalDataApi: sinon.stub().returns({ type: 'local', init: initStub }),
       RemoteDataAPI: sinon.stub().returns({ type: 'remote' }),
       CreatorDataApi: sinon.stub().returns({ type: 'creator' }),
     };
@@ -26,16 +27,18 @@ describe('dataAPI client unit tests', () => {
       CREATOR_APP_ORIGIN: 'voiceflow.com',
     };
 
-    expect(new DataAPI(config as any, API as any).get()).to.eql({ type: 'local' });
+    expect(await new DataAPI(config as any, API as any).get()).to.eql({ type: 'local', init: initStub });
     expect(API.LocalDataApi.args).to.eql([[{ projectSource: config.PROJECT_SOURCE }, { fs: Static.fs, path: Static.path }]]);
-    expect(API.CreatorDataApi.callCount).to.eql(1);
-    expect(API.RemoteDataAPI.callCount).to.eql(1);
+    expect(initStub.callCount).to.eql(1);
+    expect(API.CreatorDataApi.callCount).to.eql(0);
+    expect(API.RemoteDataAPI.callCount).to.eql(0);
   });
 
-  it('remote api', () => {
+  it('remote api', async () => {
+    const initStub = sinon.stub();
     const API = {
       LocalDataApi: sinon.stub().returns({ type: 'local' }),
-      RemoteDataAPI: sinon.stub().returns({ type: 'remote' }),
+      RemoteDataAPI: sinon.stub().returns({ type: 'remote', init: initStub }),
       CreatorDataApi: sinon.stub().returns({ type: 'creator' }),
     };
 
@@ -49,24 +52,21 @@ describe('dataAPI client unit tests', () => {
 
     const origin = 'voiceflow.com';
 
-    expect(new DataAPI(config as any, API as any).get('', origin)).to.eql({ type: 'remote' });
+    expect(await new DataAPI(config as any, API as any).get('', origin)).to.eql({ type: 'remote', init: initStub });
     expect(API.RemoteDataAPI.args).to.eql([
       [{ platform: 'general', adminToken: config.ADMIN_SERVER_DATA_API_TOKEN, dataEndpoint: config.VF_DATA_ENDPOINT }, { axios: Static.axios }],
     ]);
-    expect(API.CreatorDataApi.callCount).to.eql(1);
+    expect(initStub.callCount).to.eql(1);
+    expect(API.CreatorDataApi.callCount).to.eql(0);
     expect(API.LocalDataApi.callCount).to.eql(0);
   });
 
-  it('creator api', () => {
-    const updateAuthorizationStub = sinon.stub();
-    const creatorDataAPIReturn = {
-      type: 'creator',
-      updateAuthorization: updateAuthorizationStub,
-    };
+  it('creator api', async () => {
+    const initStub = sinon.stub();
     const API = {
       LocalDataApi: sinon.stub().returns({ type: 'local' }),
       RemoteDataAPI: sinon.stub().returns({ type: 'remote' }),
-      CreatorDataApi: sinon.stub().returns(creatorDataAPIReturn),
+      CreatorDataApi: sinon.stub().returns({ type: 'creator', init: initStub }),
     };
 
     const config = {
@@ -78,23 +78,23 @@ describe('dataAPI client unit tests', () => {
     };
 
     const origin = 'other-site.com';
-
     const dataAPI = new DataAPI(config as any, API as any);
 
-    expect(dataAPI.get('', origin)).to.eql(creatorDataAPIReturn);
+    expect(await dataAPI.get('', origin)).to.eql({ type: 'creator', init: initStub });
     expect(API.CreatorDataApi.args).to.eql([[{ endpoint: `${config.CREATOR_API_ENDPOINT}/v2`, authorization: config.CREATOR_API_AUTHORIZATION }]]);
-    expect(API.RemoteDataAPI.callCount).to.eql(1);
+    expect(initStub.callCount).to.eql(1);
     expect(API.LocalDataApi.callCount).to.eql(0);
+    expect(API.RemoteDataAPI.callCount).to.eql(0);
 
-    expect(dataAPI.get('new auth', origin)).to.eql(creatorDataAPIReturn);
-    expect(updateAuthorizationStub.secondCall.args).to.eql(['new auth']);
+    expect(await dataAPI.get('new auth', origin)).to.eql({ type: 'creator', init: initStub });
+    expect(API.CreatorDataApi.secondCall.args).to.eql([{ endpoint: `${config.CREATOR_API_ENDPOINT}/v2`, authorization: 'new auth' }]);
   });
 
   it('fails if no data API env configuration set', () => {
     expect(() => new DataAPI({} as any, {} as any)).to.throw('no data API env configuration set');
   });
 
-  it('fails if origin matches but no remote data API env configuration set', () => {
+  it('fails if origin matches but no remote data API env configuration set', async () => {
     const API = {
       LocalDataApi: sinon.stub().returns({ type: 'local' }),
       RemoteDataAPI: sinon.stub().returns({ type: 'remote' }),
@@ -108,11 +108,12 @@ describe('dataAPI client unit tests', () => {
     };
 
     const origin = 'voiceflow.com';
+    const dataAPI = new DataAPI(config as any, API as any);
 
-    expect(() => new DataAPI(config as any, API as any).get('', origin)).to.throw('no remote data API env configuration set');
+    expect(dataAPI.get('', origin)).to.be.rejectedWith('no remote data API env configuration set');
   });
 
-  it('fails if no PROJECT_SOURCE and origin does not match but no creator data API env configuration set', () => {
+  it('fails if no PROJECT_SOURCE and origin does not match but no creator data API env configuration set', async () => {
     const API = {
       LocalDataApi: sinon.stub().returns({ type: 'local' }),
       RemoteDataAPI: sinon.stub().returns({ type: 'remote' }),
@@ -126,7 +127,8 @@ describe('dataAPI client unit tests', () => {
     };
 
     const origin = 'other-site.com';
+    const dataAPI = new DataAPI(config as any, API as any);
 
-    expect(() => new DataAPI(config as any, API as any).get('', origin)).to.throw('no creator data API env configuration set');
+    expect(dataAPI.get('', origin)).to.be.rejectedWith('no creator data API env configuration set');
   });
 });
