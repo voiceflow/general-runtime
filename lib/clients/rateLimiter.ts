1f0c7cafb84007b835ed9bc05575d2ef31b23e30
Tyler Han <tyler@voiceflow.com>
2021-02-27 17:16:11 -0500

    chore: fix tests

--- lib/clients/rateLimiter.ts
+++ lib/clients/rateLimiter.ts
@@ -6,25 +6,42 @@ import { Redis } from './redis';
 
 export type RateLimiter = RateLimiterStoreAbstract;
 
+// public rate limiter - req from creator-app or clients without an authorization
+export const RATE_LIMITER_PUBLIC = 'general-runtime-rate-limiter-public';
+// private rate limiter - req from clients with authorization (auth_vf token or api key)
+export const RATE_LIMITER_PRIVATE = 'general-runtime-rate-limiter-private';
+
 export const RateLimiterClient = (
   redis: Redis | null,
   { RATE_LIMITER_POINTS_PUBLIC, RATE_LIMITER_DURATION_PUBLIC, RATE_LIMITER_POINTS_PRIVATE, RATE_LIMITER_DURATION_PRIVATE }: Config
-) => {
-  const RateLimiter = redis ? RateLimiterRedis : RateLimiterMemory;
+): { public: RateLimiter; private: RateLimiter } => {
+  if (redis) {
+    return {
+      public: new RateLimiterRedis({
+        points: RATE_LIMITER_POINTS_PUBLIC,
+        duration: RATE_LIMITER_DURATION_PUBLIC,
+        keyPrefix: RATE_LIMITER_PUBLIC,
+        storeClient: redis,
+      }),
+      private: new RateLimiterRedis({
+        points: RATE_LIMITER_POINTS_PRIVATE,
+        duration: RATE_LIMITER_DURATION_PRIVATE,
+        keyPrefix: RATE_LIMITER_PRIVATE,
+        storeClient: redis,
+      }),
+    };
+  }
+
   return {
-    // public rate limiter - req from creator-app or clients without an authorization
-    public: new RateLimiter({
+    public: new RateLimiterMemory({
       points: RATE_LIMITER_POINTS_PUBLIC,
       duration: RATE_LIMITER_DURATION_PUBLIC,
-      keyPrefix: 'general-runtime-rate-limiter-public',
-      storeClient: redis,
+      keyPrefix: RATE_LIMITER_PUBLIC,
     }),
-    // private rate limiter - req from clients with authorization (auth_vf token or api key)
-    private: new RateLimiter({
+    private: new RateLimiterMemory({
       points: RATE_LIMITER_POINTS_PRIVATE,
       duration: RATE_LIMITER_DURATION_PRIVATE,
-      keyPrefix: 'general-runtime-rate-limiter-private',
-      storeClient: redis,
+      keyPrefix: RATE_LIMITER_PRIVATE,
     }),
   };
 };
