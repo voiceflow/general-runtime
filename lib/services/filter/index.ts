7217288a205c809103e8faf44ebb6d515a7f6cc0
Zhiheng Lu <brennan@voiceflow.com>
2021-03-03 15:33:32 -0500

    feat: adding filter service

---
+++ lib/services/filter/index.ts
@@ -0,0 +1,45 @@
+/**
+ * [[include:filter.md]]
+ * @packageDocumentation
+ */
+
+import { TraceType } from '@/../general-types/build';
+import { SpeakType } from '@/../general-types/build/nodes/speak';
+import { Context, ContextHandler } from '@/types';
+
+import { AbstractManager, injectServices } from '../utils';
+import { sanitizeSSML } from './utils';
+
+export const utils = {
+  sanitizeSSML,
+};
+
+@injectServices({ utils })
+class Filter extends AbstractManager<{ utils: typeof utils }> implements ContextHandler {
+  handle(context: Context) {
+    const {
+      data: { config = {} },
+    } = context;
+
+    if (config.stripSSML) {
+      context = {
+        ...context,
+        trace: context.trace?.map((trace) =>
+          !(trace.type === TraceType.SPEAK && trace.payload.type === SpeakType.MESSAGE)
+            ? trace
+            : {
+                ...trace,
+                payload: {
+                  ...trace.payload,
+                  message: sanitizeSSML(trace.payload.message),
+                },
+              }
+        ),
+      };
+    }
+
+    return context;
+  }
+}
+
+export default Filter;
