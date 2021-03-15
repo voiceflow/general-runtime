9a611b1f3a4b79b4e667e724e9f751d859ffca2e
leartgjoni-voiceflow <leart@voiceflow.com>
2021-03-15 14:26:41 -0400

    wip: preliminary handler

--- lib/services/runtime/handlers/state/preliminary.ts
+++ lib/services/runtime/handlers/state/preliminary.ts
@@ -1,5 +1,5 @@
 import { Node } from '@voiceflow/api-sdk';
-import { HandlerFactory } from '@voiceflow/runtime';
+import { Action, HandlerFactory } from '@voiceflow/runtime';
 
 import { eventHandlers } from '..';
 import { isGeneralRequest, isIntentRequest } from '../../types';
@@ -10,12 +10,22 @@ const utilsObj = {
   eventHandlers,
 };
 
+/**
+ * If request comes in but runtime nodeID is not a node that handles events (interaction, _v1, etc..) =>
+ * Handle it here
+ */
 export const PreliminaryHandler: HandlerFactory<Node<any, any>, typeof utilsObj> = (utils) => ({
   canHandle: (node, runtime, variables, program) => {
     const request = runtime.getRequest();
-    return (isIntentRequest(request) || isGeneralRequest(request)) && !eventHandlers.find((h) => h.canHandle(node, runtime, variables, program));
+    return (
+      (isIntentRequest(request) || isGeneralRequest(request)) &&
+      !eventHandlers.find((h) => h.canHandle(node, runtime, variables, program)) &&
+      runtime.getAction() === Action.REQUEST
+    );
   },
   handle: (node, runtime, variables) => {
+    runtime.setAction(Action.RESPONSE);
+
     // check if there is a command in the stack that fulfills request
     if (utils.commandHandler.canHandle(runtime)) {
       return utils.commandHandler.handle(runtime, variables);
