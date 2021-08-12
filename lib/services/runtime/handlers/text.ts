f1cd160815bf41236f442b8590c76d3ebab338b8
Tyler Han <tyler@voiceflow.com>
2021-08-11 22:49:21 -0400

    chore: hacky implementation

--- lib/services/runtime/handlers/text.ts
+++ lib/services/runtime/handlers/text.ts
@@ -1,18 +1,33 @@
 import { replaceVariables, sanitizeVariables } from '@voiceflow/common';
 import { NodeType, TraceType } from '@voiceflow/general-types';
-import { Node, TraceFrame } from '@voiceflow/general-types/build/nodes/text';
+import { Descendant, Node, TraceFrame } from '@voiceflow/general-types/build/nodes/text';
 import _sample from 'lodash/sample';
+import { Text } from 'slate';
 
 import { HandlerFactory } from '@/runtime';
 
 const TextHandler: HandlerFactory<Node> = () => ({
   canHandle: (node) => node.type === NodeType.TEXT,
   handle: (node, runtime, variables) => {
-    const text = _sample(node.texts);
+    const slate = _sample(node.texts);
+    const sanitizedVars = sanitizeVariables(variables.getState());
 
-    if (text?.html) {
-      const sanitizedVars = sanitizeVariables(variables.getState());
-      runtime.trace.addTrace<TraceFrame>({ type: TraceType.TEXT, payload: { id: text.id, html: replaceVariables(text.html, sanitizedVars) } });
+    let text = '';
+    const injectVariables = (content: Descendant[]) => {
+      content.map((n) => {
+        if (Text.isText(n)) {
+          const newText = replaceVariables(n.text, sanitizedVars);
+          text += newText;
+          n.text = newText;
+        } else {
+          injectVariables(n.children);
+        }
+      });
+    };
+
+    if (slate) {
+      injectVariables(slate.content);
+      runtime.trace.addTrace<TraceFrame>({ type: TraceType.TEXT, payload: { slate, text } });
     }
 
     return node.nextId ?? null;
