0b540369b2d8743bb65ecce43bb5adacbf634b6b
Tyler Han <tyler@voiceflow.com>
2021-08-12 02:16:37 -0400

    chore: clean up

--- lib/services/runtime/handlers/text.ts
+++ lib/services/runtime/handlers/text.ts
@@ -4,30 +4,50 @@ import { Descendant, Node, TraceFrame } from '@voiceflow/general-types/build/nod
 import _sample from 'lodash/sample';
 import { Text } from 'slate';
 
+import log from '@/logger';
 import { HandlerFactory } from '@/runtime';
+
+const slateToPlaintext = (content: Descendant[] = []): string =>
+  content.reduce((acc, n) => {
+    acc += Text.isText(n) ? n.text : slateToPlaintext(n.children);
+    return acc;
+  }, '');
+
+const slateInjectVariables = (variables: Record<string, unknown>) => {
+  const injectVariables = (content: Descendant[] = []): Descendant[] =>
+    content.map((n) =>
+      Text.isText(n)
+        ? {
+            ...n,
+            // to counteract replaceVariables trim effect: https://github.com/voiceflow/common/blob/master/src/utils/variables.ts#L21
+            text: n.text.trim() ? replaceVariables(n.text, variables) : n.text,
+          }
+        : {
+            ...n,
+            ...(Array.isArray(n.children) && { children: injectVariables(n.children) }),
+          }
+    );
+
+  return injectVariables;
+};
 
 const TextHandler: HandlerFactory<Node> = () => ({
   canHandle: (node) => node.type === NodeType.TEXT,
   handle: (node, runtime, variables) => {
     const slate = _sample(node.texts);
-    const sanitizedVars = sanitizeVariables(variables.getState());
-
-    let text = '';
-    const injectVariables = (content: Descendant[]) => {
-      content.map((n) => {
-        if (Text.isText(n)) {
-          const newText = replaceVariables(n.text, sanitizedVars);
-          text += newText;
-          n.text = newText;
-        } else {
-          injectVariables(n.children);
-        }
-      });
-    };
 
     if (slate) {
-      injectVariables(slate.content);
-      runtime.trace.addTrace<TraceFrame>({ type: TraceType.TEXT, payload: { slate, text } });
+      try {
+        const sanitizedVars = sanitizeVariables(variables.getState());
+        const newSlate = { id: slate.id, content: slateInjectVariables(sanitizedVars)(slate.content) };
+
+        runtime.trace.addTrace<TraceFrame>({
+          type: TraceType.TEXT,
+          payload: { slate: newSlate, text: slateToPlaintext(newSlate.content) },
+        });
+      } catch (error) {
+        log.error(error);
+      }
     }
 
     return node.nextId ?? null;
