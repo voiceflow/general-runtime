d18b9597b0065134c1869105c468fcc0b63b31a4
Jonah Snider <jonah@jonahsnider.com>
2021-10-04 04:29:46 -0700

    test: add note about timestamp flakiness

--- tests/lib/services/analytics.unit.ts
+++ tests/lib/services/analytics.unit.ts
@@ -19,6 +19,7 @@ describe('analytics manager unit tests', () => {
     const context = { versionID: 1, data: '_context123' };
     expect(analytics.handle(context as any)).to.eql(context);
 
+    // TODO: This doesn't work - https://app.circleci.com/pipelines/github/voiceflow/general-runtime/1333/workflows/983fe1f5-3df4-4e88-9266-840a68f6c14b/jobs/2720
     const clock = sinon.useFakeTimers(new Date());
 
     expect(services.analyticsClient.track.args).to.eql([
