0c53884416aed2da6c37d9958b0ffc61f0d95bfd
Jonah Snider <jonah@jonahsnider.com>
2021-12-10 06:24:57 -0800

    feat: implement new metrics

--- lib/clients/metrics.ts
+++ lib/clients/metrics.ts
@@ -1,28 +1,76 @@
-import { BufferedMetricsLogger } from 'datadog-metrics';
+import { Counter, ValueRecorder } from '@opentelemetry/api-metrics';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+import { Meter, MeterProvider, MetricExporter } from '@opentelemetry/sdk-metrics-base';
 
+import log from '@/logger';
 import { Config } from '@/types';
 
 export class Metrics {
-  private client: BufferedMetricsLogger;
+  private meter: Meter;
 
-  constructor(config: Config, Logger: typeof BufferedMetricsLogger) {
-    this.client = new Logger({
-      apiKey: config.DATADOG_API_KEY,
-      prefix: `vf_general_runtime.${config.NODE_ENV}.`,
-      flushIntervalSeconds: 5,
+  private exporter: MetricExporter;
+
+  private counters: {
+    general: {
+      request: Counter;
+    };
+    sdk: {
+      request: Counter;
+    };
+  };
+
+  private recorders: {
+    httpRequestDuration: ValueRecorder;
+  };
+
+  constructor(config: Config) {
+    const port = config.PORT_METRICS ? Number(config.PORT_METRICS) : PrometheusExporter.DEFAULT_OPTIONS.port;
+    const { endpoint } = PrometheusExporter.DEFAULT_OPTIONS;
+
+    this.exporter = new PrometheusExporter({ port, endpoint }, () => {
+      log.info(`[metrics] exporter ready ${log.vars({ port, path: endpoint })}`);
     });
+
+    this.meter = new MeterProvider({ exporter: this.exporter, interval: config.NODE_ENV === 'test' ? 0 : 1000 }).getMeter('general-runtime');
+
+    this.counters = {
+      general: {
+        request: this.meter.createCounter('general_request', { description: 'General requests' }),
+      },
+      sdk: {
+        request: this.meter.createCounter('sdk_request', { description: 'SDK requests' }),
+      },
+    };
+
+    this.recorders = {
+      httpRequestDuration: this.meter.createValueRecorder('http_request_duration', { description: 'Http requests duration' }),
+    };
   }
 
-  generalRequest() {
-    this.client.increment('general.request');
+  generalRequest(): void {
+    this.counters.general.request.add(1);
   }
 
-  sdkRequest() {
-    this.client.increment('sdk.request');
+  sdkRequest(): void {
+    this.counters.sdk.request.add(1);
+  }
+
+  httpRequestDuration(operation: string, statusCode: number, opts: { duration: number }): void {
+    this.recorders.httpRequestDuration
+      .bind({
+        operation,
+        status_code: statusCode.toString(),
+      })
+      .record(opts.duration);
+  }
+
+  async stop(): Promise<void> {
+    await this.meter.shutdown();
+    await this.exporter.shutdown();
   }
 }
 
-const MetricsClient = (config: Config) => new Metrics(config, BufferedMetricsLogger);
+const MetricsClient = (config: Config) => new Metrics(config);
 
 export type MetricsType = Metrics;
 
--- tests/lib/clients/metrics.unit.ts
+++ tests/lib/clients/metrics.unit.ts
@@ -1,54 +1,76 @@
+import axios from 'axios';
 import { expect } from 'chai';
+import getPort from 'get-port';
 import _ from 'lodash';
-import sinon from 'sinon';
+import { setTimeout as sleep } from 'timers/promises';
+import types from 'util/types';
 
-import Config from '@/config';
 import MetricsClient, { Metrics } from '@/lib/clients/metrics';
 
+const baseConfig = async () => ({ PORT_METRICS: await getPort(), NODE_ENV: 'test' });
+
+const assertHelper = async ({ config = {}, expected }: { config?: Record<any, any>; expected: RegExp | ReadonlyArray<RegExp> }) => {
+  const combinedConfig = { ...(await baseConfig()), ...config };
+
+  const metrics = MetricsClient(combinedConfig as any);
+
+  const expressions = types.isRegExp(expected) ? [expected] : expected;
+
+  return {
+    metrics,
+    async assert(): Promise<void> {
+      await sleep(1);
+
+      try {
+        const { data } = await axios.get<string>(`http://localhost:${combinedConfig.PORT_METRICS}/metrics`);
+
+        expressions.forEach((expression) => {
+          expect(data).to.match(expression);
+        });
+      } finally {
+        await metrics.stop();
+      }
+    },
+  };
+};
+
 describe('metrics client unit tests', () => {
-  afterEach(() => {
-    sinon.restore();
+  it('new', async () => {
+    const config = await baseConfig();
+
+    const metrics = new Metrics(config as any);
+
+    await metrics.stop();
   });
 
-  it('new', () => {
-    const NODE_ENV = 'test';
-    const loggerStub = sinon.stub().returns({
-      increment: () => {
-        //
-      },
+  it('generalRequest', async () => {
+    const helper = await assertHelper({ expected: /^general_request_total 1 \d+$/m });
+
+    helper.metrics.generalRequest();
+
+    await helper.assert();
+  });
+
+  it('sdkRequest', async () => {
+    const helper = await assertHelper({ expected: /^sdk_request_total 1 \d+$/m });
+
+    helper.metrics.sdkRequest();
+
+    await helper.assert();
+  });
+
+  it('httpRequestDuration', async () => {
+    const helper = await assertHelper({
+      expected: [
+        /^http_request_duration_count{operation="operation",status_code="123"} 2 \d+$/m,
+        /^http_request_duration_sum{operation="operation",status_code="123"} 300 \d+$/m,
+        /^http_request_duration_bucket{operation="operation",status_code="123",le="\+Inf"} 2 \d+$/m,
+      ],
     });
 
-    const metrics = new Metrics({ NODE_ENV, DATADOG_API_KEY: 'key' } as any, loggerStub as any);
+    helper.metrics.httpRequestDuration('operation', 123, { duration: 200 });
+    helper.metrics.httpRequestDuration('operation', 123, { duration: 100 });
 
-    expect(typeof _.get(metrics, 'client.increment')).to.eql('function');
-
-    expect(loggerStub.calledWithNew()).to.eql(true);
-    expect(loggerStub.args).to.eql([
-      [
-        {
-          apiKey: 'key',
-          prefix: `vf_general_runtime.${NODE_ENV}.`,
-          flushIntervalSeconds: 5,
-        },
-      ],
-    ]);
-  });
-
-  it('generalRequest', () => {
-    const metrics = MetricsClient({} as any);
-    const increment = sinon.stub();
-    _.set(metrics, 'client', { increment });
-
-    metrics.generalRequest();
-    expect(increment.args).to.eql([['general.request']]);
-  });
-
-  it('sdkRequest', () => {
-    const metrics = MetricsClient({} as any);
-    const increment = sinon.stub();
-    _.set(metrics, 'client', { increment });
-
-    metrics.sdkRequest();
-    expect(increment.args).to.eql([['sdk.request']]);
+    await helper.assert();
   });
 });
