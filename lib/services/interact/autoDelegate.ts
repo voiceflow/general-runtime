cafb937c76fec826b4c3766129e246841b1e46a7
Tyler Han <tylerhan97@gmail.com>
2022-01-03 16:08:24 -0500

    chore: bump types

--- lib/services/interact/autoDelegate.ts
+++ lib/services/interact/autoDelegate.ts
@@ -1,34 +1,23 @@
 import { Trace } from '@voiceflow/base-types';
+import { Utils } from '@voiceflow/common';
 
 import { PartialContext, TurnBuilder } from '@/runtime';
 import { Context } from '@/types';
 
 export const MAX_DELEGATION_TURNS = 3;
 
-// mostly just saves us needing to traverse an array twice
-const filterAndGetLastRemovedValue = <A>(list: A[], filterFunc: (a: A) => boolean): [A[], A | null] => {
-  let lastItem: A | null = null;
-  const filteredList = list.filter((a) => {
-    if (filterFunc(a)) return true;
-    lastItem = a;
-    return false;
-  });
-  return [filteredList, lastItem];
-};
-
-const isGoToTrace = (frame?: Trace.AnyTrace | null): frame is Trace.GoToTrace => frame?.type === Trace.TraceType.GOTO && !!frame.payload.request;
+const isGoToTrace = (frame: Trace.AnyTrace | null): frame is Trace.GoToTrace => frame?.type === Trace.TraceType.GOTO && !!frame.payload.request;
 
 const autoDelegateTurn = async (turn: TurnBuilder<Context>, initContext: PartialContext<Context>): Promise<Context> => {
-  let iterations = 0;
   let context: Context | null = null;
   const trace: Trace.AnyTrace[] = [];
 
-  do {
+  for (let i = 0; i < MAX_DELEGATION_TURNS; i++) {
     // eslint-disable-next-line no-await-in-loop
     context = await turn.handle(context || initContext);
     if (!context.trace) break;
 
-    const [filteredTrace, goToTrace] = filterAndGetLastRemovedValue(context.trace, (frame) => !isGoToTrace(frame));
+    const [filteredTrace, goToTrace] = Utils.array.filterAndGetLastRemovedValue(context.trace, (frame) => !isGoToTrace(frame));
     trace.push(...filteredTrace);
 
     if (isGoToTrace(goToTrace)) {
@@ -36,11 +25,9 @@ const autoDelegateTurn = async (turn: TurnBuilder<Context>, initContext: Partial
     } else {
       break;
     }
+  }
 
-    iterations++;
-  } while (iterations < MAX_DELEGATION_TURNS);
-
-  return { ...context, trace };
+  return { ...context!, trace };
 };
 
 export default autoDelegateTurn;
