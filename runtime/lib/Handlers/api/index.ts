f554840ce60210bfee3b9d188d0d4d8dd49bb244
Tyler Han <tylerhan97@gmail.com>
2022-01-13 15:46:13 -0500

    fix: api injection (VF-000)

--- runtime/lib/Handlers/api/index.ts
+++ runtime/lib/Handlers/api/index.ts
@@ -5,15 +5,11 @@ import safeJSONStringify from 'safe-json-stringify';
 
 import { HandlerFactory } from '@/runtime/lib/Handler';
 
-import { APINodeData, makeAPICall } from './utils';
-
-export type IntegrationsOptions = {
-  customAPIEndpoint?: string | null;
-};
+import { APINodeData, makeAPICall, ResponseConfig } from './utils';
 
 export const USER_AGENT_KEY = 'User-Agent';
 export const USER_AGENT = 'Voiceflow/1.0.0 (+https://voiceflow.com)';
-const APIHandler: HandlerFactory<Node.Integration.Node, IntegrationsOptions | void> = () => ({
+const APIHandler: HandlerFactory<Node.Integration.Node, ResponseConfig | void> = (config = {}) => ({
   canHandle: (node) => node.type === Node.NodeType.INTEGRATIONS && node.selected_integration === Node.Utils.IntegrationType.CUSTOM_API,
   handle: async (node, runtime, variables) => {
     let nextId: string | null = null;
@@ -26,7 +22,7 @@ const APIHandler: HandlerFactory<Node.Integration.Node, IntegrationsOptions | vo
         actionBodyData.headers = [...headers, { key: USER_AGENT_KEY, val: USER_AGENT }];
       }
 
-      const data = await makeAPICall(actionBodyData, runtime);
+      const data = await makeAPICall(actionBodyData, runtime, config as ResponseConfig);
 
       // add mapped variables to variables store
       variables.merge(data.variables);
