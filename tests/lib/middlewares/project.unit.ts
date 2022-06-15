a7abb2c6ff95cce53358ec64d10d90e5bbae4fb7
Brennan Lu <brennan.lu314@gmail.com>
2022-06-15 15:34:41 -0400

    test: unifyVersionID

--- tests/lib/middlewares/project.unit.ts
+++ tests/lib/middlewares/project.unit.ts
@@ -6,13 +6,80 @@ import sinon from 'sinon';
 import Project from '@/lib/middlewares/project';
 
 describe('project middleware unit tests', () => {
-  const getMockRequest = <P, B, H>({ params, body, headers }: { params?: P; body?: B; headers?: H } = {}): Request<
+  const getMockRequest = <P, RB, B, H>({ params, body, headers }: { params?: P; body?: B; headers?: H } = {}): Request<
     P,
+    RB,
     B,
     H
   > => ({ params, body, headers } as any);
   const getMockResponse = (): Response => ({} as any);
   const getMockNext = () => sinon.fake();
+
+  const versionID1 = 'xyz';
+  const versionID2 = 'abc';
+
+  describe('unifyVersionID', () => {
+    it('adds versionID to header if it exists on params', async () => {
+      // arrange
+      const middleware = new Project({} as any, {} as any);
+
+      const req = getMockRequest({
+        headers: { versionID: versionID1 },
+        params: { versionID: versionID2 },
+        body: null,
+      });
+      const res = getMockResponse();
+      const next = getMockNext();
+
+      // act
+      await middleware.unifyVersionID(req, res, next);
+
+      // assert
+      expect(next.callCount).to.equal(1);
+      expect(next.args[0].length).to.equal(0);
+      expect(req.headers.versionID).to.equal(versionID2);
+    });
+
+    it('does nothing if versionID is only specified on header', async () => {
+      // arrange
+      const middleware = new Project({} as any, {} as any);
+
+      const req = getMockRequest({
+        headers: { versionID: versionID1 },
+        params: {},
+        body: null,
+      });
+      const res = getMockResponse();
+      const next = getMockNext();
+
+      // act
+      await middleware.unifyVersionID(req, res, next);
+
+      // assert
+      expect(next.callCount).to.equal(1);
+      expect(next.args[0].length).to.equal(0);
+      expect(req.headers.versionID).to.equal(versionID1);
+    });
+
+    it('throws error if no versionID in header or params', async () => {
+      // arrange
+      const middleware = new Project({} as any, {} as any);
+
+      const req = getMockRequest({
+        headers: {},
+        params: {},
+        body: null,
+      });
+      const res = getMockResponse();
+      const next = getMockNext();
+
+      // act
+      const result = middleware.unifyVersionID(req, res, next);
+
+      // assert
+      await expect(result).to.be.eventually.rejectedWith('Missing versionID in request');
+    });
+  });
 
   describe('resolveVersionAlias', () => {
     it('does not look up alias if version ID is not an alias tag', async () => {
