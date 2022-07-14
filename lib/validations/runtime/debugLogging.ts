e205a5def9fe2833c89fef0dc994622f3d57fb03
Jonah Snider <jonah@jonahsnider.com>
2022-07-13 17:25:20 -0700

    feat: remove runtime logging feature flag

--- lib/validations/runtime/debugLogging.ts
+++ lib/validations/runtime/debugLogging.ts
@@ -39,13 +39,8 @@ const resolveLogLevel = (value: LogLevelResolvable): RuntimeLogs.LogLevel => {
 const { query } = Validator;
 
 export const QUERY = {
-  LOGS: CONFIG.FF_RUNTIME_LOGGING
-    ? query('logs')
-        .custom((value: unknown) => isLogLevelResolvable(value))
-        .withMessage('must be a known log level, boolean, or undefined')
-        .customSanitizer(resolveLogLevel)
-    : query('logs')
-        .custom((value: unknown) => value === undefined)
-        .withMessage('The runtime logging feature flag is not enabled')
-        .customSanitizer(() => RuntimeLogs.LogLevel.OFF),
+  LOGS: query('logs')
+    .custom((value: unknown) => isLogLevelResolvable(value))
+    .withMessage('must be a known log level, boolean, or undefined')
+    .customSanitizer(resolveLogLevel),
 };
