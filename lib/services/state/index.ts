cecfa30d84e80c539ed8b089159e398562841a08
Tyler Han <tylerhan97@gmail.com>
2023-04-11 17:06:36 -0400

    fix: things

--- lib/services/state/index.ts
+++ lib/services/state/index.ts
@@ -4,7 +4,6 @@ import _ from 'lodash';
 import { PartialContext, State } from '@/runtime';
 import { Context, InitContextHandler } from '@/types';
 
-import AIAssist from '../aiAssist';
 import { AbstractManager, injectServices } from '../utils';
 import CacheDataAPI from './cacheDataAPI';
 
