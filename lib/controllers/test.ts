68aab2a3ac44f94be1826ac734518698bfae1bd5
Tyler Han <tylerhan97@gmail.com>
2023-04-17 11:07:02 -0400

    fix: remove validate

--- lib/controllers/test.ts
+++ lib/controllers/test.ts
@@ -1,39 +1,12 @@
-import Voiceflow from '@voiceflow/api-sdk';
-import VError from '@voiceflow/verror';
-
 import { getAPIBlockHandlerOptions } from '@/lib/services/runtime/handlers/api';
-import log from '@/logger';
 import { callAPI } from '@/runtime/lib/Handlers/api/utils';
 import { ivmExecute } from '@/runtime/lib/Handlers/code/utils';
 import { Request, Response } from '@/types';
 
 import { AbstractController } from './utils';
 
-const VF_COOKIE = 'auth_vf';
-
 class TestController extends AbstractController {
-  // validate API with creator-api
-  // TODO: switch to auth in the future
-  validate = async (cookies: { [VF_COOKIE]?: string }) => {
-    const authorization = cookies[VF_COOKIE];
-    if (!authorization) {
-      throw new VError('missing auth cookie', VError.HTTP_STATUS.UNAUTHORIZED);
-    }
-    const client = new Voiceflow({ apiEndpoint: this.config.CREATOR_API_ENDPOINT!, clientKey: '' }).generateClient({
-      authorization,
-    });
-
-    try {
-      await client.fetch.get('/session');
-    } catch (error) {
-      log.warn(error);
-      throw new VError('invalid session', VError.HTTP_STATUS.UNAUTHORIZED);
-    }
-  };
-
   async testAPI(req: Request, res: Response) {
-    await this.validate(req.cookies);
-
     const { responseJSON } = await callAPI(req.body.api, getAPIBlockHandlerOptions(this.config));
     if (responseJSON.VF_STATUS_CODE) {
       res.status(responseJSON.VF_STATUS_CODE);
@@ -42,8 +15,6 @@ class TestController extends AbstractController {
   }
 
   async testCode(req: Request, res: Response) {
-    await this.validate(req.cookies);
-
     if (typeof req.body.code !== 'string') {
       res.status(400).send({ error: 'code must be a string' });
       return;
