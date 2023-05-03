d2af0aaa9ea813e83bce75851961a38375a285ae
Tyler Han <tylerhan97@gmail.com>
2023-05-03 17:03:22 -0400

    fix: serve

--- lib/services/runtime/handlers/utils/knowledgeBaseNoMatch.ts
+++ lib/services/runtime/handlers/utils/knowledgeBaseNoMatch.ts
@@ -1,4 +1,4 @@
-import { HTTP_STATUS } from '@voiceflow/verror';
+import { BaseUtils } from '@voiceflow/base-types';
 import axios from 'axios';
 
 import Config from '@/config';
@@ -7,7 +7,22 @@ import log from '@/logger';
 import { Runtime } from '@/runtime';
 
 import { Output } from '../../types';
+import { fetchChat, fetchPrompt, getMemoryMessages, Message } from './ai';
 import { generateOutput } from './output';
+
+interface KnowledegeBaseChunk {
+  score: number;
+  chunkID: string;
+  documentID: string;
+  originalText: string;
+}
+interface KnowledgeBaseResponse {
+  chunks: KnowledegeBaseChunk[];
+}
+
+const createKnowledgeString = ({ chunks }: KnowledgeBaseResponse) => {
+  return chunks.map((chunk, index) => `${index + 1}: \n\n"${chunk.originalText}"\n\n`).join('');
+};
 
 export const knowledgeBaseNoMatch = async (runtime: Runtime): Promise<Output | null> => {
   if (!Config.KNOWLEDGE_BASE_LAMBDA_ENDPOINT) {
@@ -22,19 +37,84 @@ export const knowledgeBaseNoMatch = async (runtime: Runtime): Promise<Output | n
   if (!inputUtterance) return null;
 
   try {
-    const response = await axios.post(answerEndpoint, {
+    // expiremental module, frame the question
+    const memory = getMemoryMessages(runtime.variables.getState());
+
+    // add question mark if not present
+    let question = inputUtterance;
+
+    if (memory.length > 1) {
+      const contextMessages: Message[] = [
+        ...memory,
+        {
+          role: 'user',
+          content: 'frame the statement above so that it can be asked as a question to someone with no context.',
+        },
+      ];
+      const response = await fetchChat({ model: BaseUtils.ai.GPT_MODEL.GPT_3_5_turbo, messages: contextMessages }, {});
+
+      if (response.output) {
+        question = response.output;
+      }
+    }
+
+    const { data } = await axios.post<KnowledgeBaseResponse>(answerEndpoint, {
       projectID: runtime.project?._id,
-      question: inputUtterance,
+      question,
       settings: runtime.project?.knowledgeBase?.settings,
     });
 
-    if (!response?.data) return null;
+    if (!data?.chunks?.length) return null;
 
-    return generateOutput(response.data.llmResponse, runtime.project);
-  } catch (err) {
-    if (err.response.status !== HTTP_STATUS.NOT_FOUND) {
-      log.error(`[knowledgeBase] ${log.vars({ err })}`);
+    const summarization = runtime.project?.knowledgeBase?.settings?.summarization;
+    const options = { ...summarization, model: summarization?.model || BaseUtils.ai.GPT_MODEL.GPT_3_5_turbo };
+
+    // if the user injects a custom prompt
+    let prompt = summarization?.prompt ? `${summarization.prompt}\n` : '';
+    let response: { output: string | null };
+
+    const variables = runtime.variables.getState();
+
+    if (!BaseUtils.ai.ChatModels.includes(options.model)) {
+      // for GPT-3 completion model
+      prompt += `Use the following documents as reference:\n\n${createKnowledgeString(data)}\n\nQ: ${question}\nA: `;
+
+      response = await fetchPrompt({ ...options, prompt, mode: BaseUtils.ai.PROMPT_MODE.PROMPT }, variables);
+    } else {
+      // for GPT-3.5 and 4.0 chat models
+
+      prompt += `Use the following documents as reference:\n\n${createKnowledgeString(data)}\n\n`;
+
+      const messages: Message[] = [
+        { role: 'user', content: prompt },
+        { role: 'user', content: question },
+      ];
+
+      // TODO: memory depends on the size of the prompt
+      response = await fetchChat({ ...options, messages }, variables);
     }
+
+    const { output, ...meta } = response;
+
+    const documents = runtime.project?.knowledgeBase?.documents || {};
+
+    if (!output) return null;
+
+    runtime.trace.addTrace({
+      type: 'knowledgeBase',
+      payload: {
+        chunks: data.chunks.map(({ score, documentID }) => ({
+          score,
+          documentID,
+          documentData: documents[documentID]?.data,
+        })),
+        ...meta,
+      },
+    } as any);
+
+    return generateOutput(output, runtime.project);
+  } catch (err) {
+    log.error(`[knowledgeBase] ${log.vars({ err })}`);
     return null;
   }
 };
