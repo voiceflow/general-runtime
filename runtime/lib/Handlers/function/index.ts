d7b177ea8a860d033f9517a7b3b6cc837f3d5a9d
Brennan Lu <brennan.lu314@gmail.com>
2023-06-30 11:24:08 -0400

    feat: error logging for the code sandbox

--- runtime/lib/Handlers/function/index.ts
+++ runtime/lib/Handlers/function/index.ts
@@ -1,17 +1,58 @@
 import { BaseNode } from '@voiceflow/base-types';
+import safeJSONStringify from 'json-stringify-safe';
 
 import Handler from '../../Handler';
 import { FunctionHandlerConfig } from './functionHandler.interface';
 import { PathCode } from './functionHandler.types';
 import { Sandbox } from './sandbox/sandbox';
 
+function sanitizeOutput(output: unknown) {
+  if (typeof output !== 'object') return {};
+  if (output === null) return {};
+  return output;
+}
+
+function diffChanges(oldVariableState: Record<string, any>, updatePatch: Record<string, any>) {
+  // The changes (a diff) that the execution of this code made to the variables
+  const updatedKeys = Object.keys(updatePatch);
+  const updateKeysSet = new Set(updatedKeys);
+  const changes: Array<[string, { before: any; after: any }]> = Object.entries(oldVariableState)
+    .filter(([key, _]) => updateKeysSet.has(key))
+    .map(([key, oldValue]) => [
+      key,
+      {
+        before: oldValue,
+        after: updatePatch[key],
+      },
+    ]);
+
+  return changes
+    .map(
+      ([variable, change]) =>
+        `\`{${variable}}\`: \`${JSON.stringify(change.before)}\` => \`${JSON.stringify(change.after)}\``
+    )
+    .join('\n');
+}
+
 export const FunctionHandler = (config: FunctionHandlerConfig): Handler<BaseNode.Code.Node> => ({
   canHandle: (node) => node.type === BaseNode.NodeType.CODE,
-  handle: async (node, _runtime, variables) => {
+  handle: async (node, runtime, variables) => {
     try {
-      const { port, output } = await Sandbox.execute(node.code, variables.getState(), {
+      const variableState = variables.getState();
+
+      const { port, output: rawOutput } = await Sandbox.execute(node.code, variableState, {
         shouldEnableInject: config.shouldInjectLog,
       });
+
+      const output = sanitizeOutput(rawOutput);
+
+      const changesSummary = diffChanges(variableState, output);
+
+      runtime.trace.debug(
+        // eslint-disable-next-line sonarjs/no-nested-template-literals
+        `evaluating code - ${changesSummary ? `changes:\n${changesSummary}` : 'no variable changes'}`,
+        BaseNode.NodeType.CODE
+      );
 
       variables.merge(output);
 
@@ -28,6 +69,11 @@ export const FunctionHandler = (config: FunctionHandlerConfig): Handler<BaseNode
 
       return node.success_id ?? null;
     } catch (error) {
+      const serializedError = error.response?.data || error.toString();
+      const stringifiedError = safeJSONStringify(serializedError).replace('isolated-vm', 'sandbox');
+
+      runtime.trace.debug(`unable to resolve code  \n\`${stringifiedError}\``, BaseNode.NodeType.CODE);
+
       return node.fail_id ?? null;
     }
   },
