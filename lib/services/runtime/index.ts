5dc590d4e7cdbf3eed32a5180ebe0917ceea2abd
Tyler Han <tylerhan97@gmail.com>
2023-08-01 13:25:54 -0400

    fix: math max

--- lib/services/runtime/index.ts
+++ lib/services/runtime/index.ts
@@ -103,7 +103,7 @@ class RuntimeManager extends AbstractManager<{ utils: typeof utils }> implements
       version: context.version,
       project: context.project,
       plan: context.plan,
-      timeout: this.config.ERROR_RESPONSE_MS - 5000,
+      timeout: Math.max(this.config.ERROR_RESPONSE_MS - 5000, 0),
     });
 
     runtime.debugLogging.refreshContext(context);
