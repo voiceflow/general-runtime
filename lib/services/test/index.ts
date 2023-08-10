95d79f5d939a120073d84718d6e71c5efdb1b5c8
Brennan Lu <brennan.lu314@gmail.com>
2023-08-10 15:42:08 -0400

    refactor: removing unnecessary skeleton file

--- lib/services/test/index.ts
+++ lib/services/test/index.ts
@@ -1,8 +1,11 @@
+import log from '@/logger';
+
 import { AbstractManager } from '../utils';
-import { FunctionRunner } from './test-runner/function.runner';
 
 export class TestService extends AbstractManager {
   public async testFunction(functionID: string, inputMapping: Record<string, unknown>) {
-    return FunctionRunner(functionID, inputMapping);
+    log.warn(`TestService.testFunction was called but is not implemented`);
+    log.info(`Retrieving function associated with functionID=${functionID}`);
+    log.info(`Using inputMapping = ${inputMapping}`);
   }
 }
--- lib/services/test/test-runner/function.runner.ts
+++
@@ -1,7 +0,0 @@
-import log from '@/logger';
-
-export function FunctionRunner(functionID: string, inputMapping: Record<string, unknown>) {
-  log.warn(`TestService.testFunction was called but is not implemented`);
-  log.info(`Retrieving function associated with functionID=${functionID}`);
-  log.info(`Using inputMapping = ${inputMapping}`);
-}
