1dcf5e7b29c605718738b764a1579e67c1734d50
Brennan Lu <brennan.lu314@gmail.com>
2023-08-10 11:11:14 -0400

    fix: url

--- backend/api/routers/test.ts
+++ backend/api/routers/test.ts
@@ -12,7 +12,7 @@ export default (middlewares: MiddlewareMap, controllers: ControllerMap) => {
   router.use(middlewares.rateLimit.verify);
 
   router.post(
-    '/workspaces/:workspaceID/functions/:functionID/',
+    '/workspaces/:workspaceID/functions/:functionID',
     middlewares.auth.authorize(['workspace:READ']),
     middlewares.rateLimit.consumeResource((req) => req.headers.authorization || req.cookies.auth_vf, 'function'),
     controllers.test.testFunction
