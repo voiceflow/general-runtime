5fd990f5b5f2fe147702a5430e93a650b9140a5e
Greg O'Grady <gregory.ogrady@voiceflow.com>
2024-04-18 15:46:28 -0400

    Revert "feat: llm intent classification (PL-844) (#783)"
    This reverts commit 4cf7ab5319de2420dd65c3b3f81bca8507a4a4f0.

--- lib/services/classification/classification.utils.ts
+++ lib/services/classification/classification.utils.ts
@@ -1,15 +1,11 @@
-import { BaseModels } from '@voiceflow/base-types';
 import {
   IntentClassificationLLMSettings,
   IntentClassificationNLUSettings,
   IntentClassificationSettings,
-  IntentClassificationSettingsDTO,
   PrototypeModel,
   Version,
 } from '@voiceflow/dtos';
-import { VoiceflowProject, VoiceflowVersion } from '@voiceflow/voiceflow-types';
-
-import { DEFAULT_NLU_INTENT_CLASSIFICATION, LEGACY_LLM_INTENT_CLASSIFICATION } from './classification.const';
+import { VoiceflowVersion } from '@voiceflow/voiceflow-types';
 
 export const isIntentClassificationNLUSettings = (
   settings: IntentClassificationSettings
@@ -23,42 +19,17 @@ export const isIntentClassificationLLMSettings = (
   return settings.type === 'llm';
 };
 
-export const isIntentClassificationSettings = (
-  intentClassificationSettings?: unknown
-): intentClassificationSettings is IntentClassificationSettings => {
-  return IntentClassificationSettingsDTO.safeParse(intentClassificationSettings).success;
-};
-
 export const castToDTO = (
-  version: VoiceflowVersion.Version,
-  project: VoiceflowProject.Project
+  version: VoiceflowVersion.Version
 ): {
-  intentClassificationSettings: IntentClassificationSettings;
+  settings: Version['settings'];
   intents?: PrototypeModel['intents'];
   slots?: PrototypeModel['slots'];
 } => {
   const { settings, prototype } = version as unknown as Version;
   const { intents, slots } = prototype?.model ?? {};
-
-  let intentClassificationSettings: IntentClassificationSettings;
-
-  if (settings?.intentClassification && isIntentClassificationSettings(settings.intentClassification)) {
-    intentClassificationSettings = settings.intentClassification;
-  } else if (project.nluSettings?.classifyStrategy === BaseModels.Project.ClassifyStrategy.VF_NLU_LLM_HYBRID) {
-    // remove after migration PL-846
-    intentClassificationSettings = LEGACY_LLM_INTENT_CLASSIFICATION;
-  } else if (version?.platformData?.settings?.intentConfidence) {
-    // remove after migration PL-846
-    intentClassificationSettings = {
-      type: 'nlu',
-      params: { confidence: version.platformData.settings.intentConfidence },
-    };
-  } else {
-    intentClassificationSettings = DEFAULT_NLU_INTENT_CLASSIFICATION;
-  }
-
   return {
-    intentClassificationSettings,
+    settings,
     intents,
     slots,
   };
