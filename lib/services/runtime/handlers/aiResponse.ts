d37990c2c9f2d812ac76ac61d9d6618eafa924bc
Tyler Stewart <git@tylerstewart.ca>
2024-04-19 19:14:27 -0600

    fix: add tokens to traces

--- lib/services/runtime/handlers/aiResponse.ts
+++ lib/services/runtime/handlers/aiResponse.ts
@@ -133,6 +133,11 @@ const AIResponseHandler: HandlerFactory<VoiceNode.AIResponse.Node, void, General
               type: BaseTrace.TraceType.COMPLETION_CONTINUE,
               payload: {
                 completion: completion.output,
+                tokens: {
+                  answer: completion.answerTokens,
+                  query: completion.queryTokens,
+                  total: completion.tokens,
+                },
               },
             };
 
@@ -146,6 +151,12 @@ const AIResponseHandler: HandlerFactory<VoiceNode.AIResponse.Node, void, General
                   voice: node.voice ?? getVersionDefaultVoice(runtime.version),
                 }),
                 type: !isChatProject(runtime.project) ? BaseTrace.TraceType.SPEAK : BaseTrace.TraceType.TEXT,
+                tokens: {
+                  model: completion.model,
+                  answer: completion.answerTokens,
+                  query: completion.queryTokens,
+                  total: completion.tokens,
+                },
               },
             };
 
