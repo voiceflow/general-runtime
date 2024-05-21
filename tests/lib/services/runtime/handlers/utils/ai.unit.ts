41263effca4f997f2c336944706f2249546c88f8
Pedro Carvalho <pedro.matosdecarvalho@voiceflow.com>
2024-05-21 17:42:30 +0100

    feat: test fixes (PL-1051)

--- tests/lib/services/runtime/handlers/utils/ai.unit.ts
+++ tests/lib/services/runtime/handlers/utils/ai.unit.ts
@@ -16,7 +16,7 @@ describe('runtime handler utils unit tests', () => {
 
     it('returns true if runtime plan supports GPT-4', () => {
       expect(canUseModel(AIModel.GPT_4 as any, { plan: PlanType.PRO } as Runtime)).to.eql(true);
-      expect(canUseModel(AIModel.GPT_4_O as any, { plan: PlanType.PRO } as Runtime)).to.eql(true);
+      expect(canUseModel(AIModel.GPT_4O as any, { plan: PlanType.PRO } as Runtime)).to.eql(true);
       expect(canUseModel(AIModel.GPT_4_TURBO as any, { plan: PlanType.PRO } as Runtime)).to.eql(true);
     });
 
@@ -24,7 +24,7 @@ describe('runtime handler utils unit tests', () => {
       const runtime = { subscriptionEntitlements: [] as SubscriptionEntitlements } as Runtime;
 
       expect(canUseModel(AIModel.GPT_4 as any, runtime)).to.eql(false);
-      expect(canUseModel(AIModel.GPT_4_O as any, runtime)).to.eql(false);
+      expect(canUseModel(AIModel.GPT_4O as any, runtime)).to.eql(false);
       expect(canUseModel(AIModel.GPT_4_TURBO as any, runtime)).to.eql(false);
     });
 
@@ -37,12 +37,12 @@ describe('runtime handler utils unit tests', () => {
       } as Runtime;
 
       const gpt4OEnabledRuntime = {
-        subscriptionEntitlements: [{ feature_id: 'feat-model-gpt-4-o', value: 'true' }],
+        subscriptionEntitlements: [{ feature_id: 'feat-model-gpt-4o', value: 'true' }],
       } as Runtime;
 
       expect(canUseModel(AIModel.GPT_4 as any, gpt4EnabledRuntime)).to.eql(true);
       expect(canUseModel(AIModel.GPT_4_TURBO as any, gpt4TurboEnabledRuntime)).to.eql(true);
-      expect(canUseModel(AIModel.GPT_4_O as any, gpt4OEnabledRuntime)).to.eql(true);
+      expect(canUseModel(AIModel.GPT_4O as any, gpt4OEnabledRuntime)).to.eql(true);
     });
   });
 });
