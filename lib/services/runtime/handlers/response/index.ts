cce49c684966316f3d237cec173b026215795be2
Brennan Lu <brennan.lu314@gmail.com>
2024-05-22 11:36:39 -0400

    refactor: cleaning up response step handler implementation

--- lib/services/runtime/handlers/response/index.ts
+++ lib/services/runtime/handlers/response/index.ts
@@ -1,4 +1,4 @@
-import { CompiledResponseNode, NodeType, ResponseVariantType, VersionProgramResources } from '@voiceflow/dtos';
+import { CompiledResponseNode, NodeType, ResponseVariantType, Version } from '@voiceflow/dtos';
 
 import { HandlerFactory } from '@/runtime';
 
@@ -9,21 +9,43 @@ const handlerUtils = {
   textOutputTrace,
 };
 
+const RESPONSE_HANDLER_ERROR_TAG = 'response-handler';
+
+const DEFAULT_DISCRIMINATOR = 'default:en-us';
+
 export const ResponseHandler: HandlerFactory<CompiledResponseNode, typeof handlerUtils> = (utils) => ({
   canHandle: (node) => node.type === NodeType.RESPONSE,
 
   handle: async (node, runtime, variables): Promise<string | null> => {
-    const { programResources } = runtime.version as any;
-    const { responses } = programResources as VersionProgramResources;
-    const responseData = responses[node.data.responseID];
-    const defaultDiscriminator = 'default:en-us';
-    const variants = responseData.variants[defaultDiscriminator];
+    if (!runtime.version) {
+      throw new Error(`[${RESPONSE_HANDLER_ERROR_TAG}]: Runtime was not loaded with a version`);
+    }
+    /**
+     * !TODO! - Need to replace this `as Version` cast by refactoring `general-runtime` to use the
+     *          `Version` from the DTOs.
+     */
+    const version = runtime.version as Version;
 
-    variants?.forEach((val) => {
+    if (!version.programResources) {
+      throw new Error(`[${RESPONSE_HANDLER_ERROR_TAG}]: Version was not compiled`);
+    }
+    const { responses } = version.programResources;
+
+    const responseData = responses[node.data.responseID];
+
+    const variants = responseData.variants[DEFAULT_DISCRIMINATOR];
+
+    if (!variants) {
+      throw new Error(
+        `[${RESPONSE_HANDLER_ERROR_TAG}]: could not resolve response step, missing variants list for '${DEFAULT_DISCRIMINATOR}'`
+      );
+    }
+
+    variants.forEach((val) => {
       if (val.type === ResponseVariantType.TEXT) {
         const trace = utils.textOutputTrace({
           output: val.data.text,
-          delay: val.data.speed ?? 50,
+          ...(val.data.speed && { delay: val.data.speed }),
           variables,
           version: runtime.version,
         });
@@ -31,10 +53,6 @@ export const ResponseHandler: HandlerFactory<CompiledResponseNode, typeof handle
         utils.addOutputTrace(runtime, trace, { node, variables });
       }
     });
-
-    if (!variants) {
-      throw new Error(`[response-handler]: could not resolve response step, missing variants fors 'default:en-us'`);
-    }
 
     return node.ports.default;
   },
