4d418c9ad0beeab31ac14adcf1db2d11dcb89b39
Tyler <tyler@Tylers-MacBook-Pro.local>
2024-05-23 09:15:38 -0400

    Revert "feat: support streaming with knowledge base (PL-987) (#814)"
    This reverts commit 4e4216a891087d1a27d3c9a414e2cd003d82e9c6.

--- lib/services/aiSynthesis/utils.ts
+++ lib/services/aiSynthesis/utils.ts
@@ -93,5 +93,3 @@ export const removePromptLeak = (output: string | null) => {
 
   return output?.replace(regex_prompt_leak, '') || null;
 };
-
-export const NOT_FOUND_RESPONSES = ['NOT_FOUND', 'NOTFOUND', "I'M SORRY,", 'AS AN AI'];
