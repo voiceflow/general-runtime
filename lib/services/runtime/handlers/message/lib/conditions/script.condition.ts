5db32e3f93b5addfe3b8f80145830bda3b0b0a6e
Brennan Lu <brennan.lu314@gmail.com>
2024-06-14 11:47:53 -0400

    feat: added skeleton framework for conditions

--- lib/services/runtime/handlers/message/lib/conditions/script.condition.ts
+++ lib/services/runtime/handlers/message/lib/conditions/script.condition.ts
@@ -1,13 +1,22 @@
 import { CompiledScriptCondition } from '@voiceflow/dtos';
+import { NotImplementedException } from '@voiceflow/exception';
 
 import { BaseCondition } from './base.condition';
 
 export class ScriptCondition extends BaseCondition<CompiledScriptCondition> {
   private evaluateCode(_code: string): boolean {
-    throw new Error('script condition is not implemented');
+    // 1 - Setup `isolated-vm` code execution sandbox
+    // 2 - Evaluate the code and pull out the result
+    // NOTE - Consider batching together code executions for a speed optimization, rather than incurring the
+    //        cost of `isolated-vm` setup every single time.
+    throw new NotImplementedException('expression condition is not implemented');
   }
 
-  public evaluate(): boolean {
-    return this.evaluateCode(this.condition.data.code);
+  evaluate(): boolean {
+    const result = this.evaluateCode(this.condition.data.code);
+
+    this.runtime.trace.debug('--- evaluated script condition --- code = {}, result = {}');
+
+    return result;
   }
 }
