e5236a070cfef2dcb10fbdc1ddb3a566c6669d80
Brennan Lu <brennan.lu314@gmail.com>
2024-06-20 15:59:05 -0400

    feat: add script condition

--- lib/services/runtime/handlers/message/lib/conditions/conditionIsolate.ts
+++ lib/services/runtime/handlers/message/lib/conditions/conditionIsolate.ts
@@ -5,6 +5,12 @@ export class ConditionIsolate {
 
   private context?: ivm.Context;
 
+  private static readonly userFunctionName = `userFunction`;
+
+  private static readonly userModuleName = `userModule`;
+
+  private static readonly userVariablesName = `inputVars`;
+
   constructor(private readonly variables: Record<string, unknown>) {}
 
   private readonly ISOLATED_VM_LIMITS = {
@@ -27,6 +33,43 @@ export class ConditionIsolate {
   public async cleanup(): Promise<void> {
     this.context?.release();
     this.isolate?.dispose();
+  }
+
+  public async executeUserModule(code: string) {
+    if (!this.isolate || !this.context) {
+      throw new Error(`condition isolate was not initialized before an attempt to execute code`);
+    }
+
+    const userModule = await this.isolate.compileModule(code);
+    await userModule.instantiate(this.context, () => {
+      throw new Error(`User code cannot import modules.`);
+    });
+
+    this.context.global.set(ConditionIsolate.userVariablesName, this.variables, { copy: true });
+
+    const sanitizeVarsFromGlobal = `
+            const ${ConditionIsolate.userVariablesName} = globalThis.${ConditionIsolate.userVariablesName};
+            delete globalThis.${ConditionIsolate.userVariablesName};
+        `;
+
+    const mainModule = await this.isolate.compileModule(`
+      import userFunction from '.'
+      ${sanitizeVarsFromGlobal}
+      (function () {
+        return ${ConditionIsolate.userFunctionName}({ ${ConditionIsolate.userVariablesName} });
+      })();
+    `);
+
+    await mainModule.instantiate(this.context, (specifier: string) => {
+      if (specifier === ConditionIsolate.userModuleName) {
+        return userModule;
+      }
+      throw new Error(`Module '${specifier}' does not exist.`);
+    });
+
+    return mainModule.evaluate({
+      timeout: this.ISOLATED_VM_LIMITS.maxExecutionTimeMs,
+    });
   }
 
   public async executeCode(code: string): Promise<unknown> {
--- lib/services/runtime/handlers/message/lib/conditions/script.condition.ts
+++ lib/services/runtime/handlers/message/lib/conditions/script.condition.ts
@@ -1,13 +1,30 @@
 import { CompiledScriptCondition } from '@voiceflow/dtos';
 
 import { BaseCondition } from './base.condition';
+import { ConditionIsolate } from './conditionIsolate';
 
 export class ScriptCondition extends BaseCondition<CompiledScriptCondition> {
-  private evaluateCode(_code: string): boolean {
-    throw new Error('script condition is not implemented');
+  private async evaluateCode(isolate: ConditionIsolate, code: string): Promise<boolean> {
+    const result = await isolate.executeCode(code);
+    return !!result;
   }
 
-  public evaluate(): boolean {
-    return this.evaluateCode(this.condition.data.code);
+  public async evaluate(): Promise<boolean> {
+    const isolate = new ConditionIsolate(this.variables);
+    try {
+      return await this.evaluateCode(isolate, this.condition.data.code);
+    } catch (err) {
+      if (err instanceof Error) {
+        throw new Error(`an error occurred executing an script condition, msg = ${err.message}`);
+      }
+      throw new Error(
+        `an unknown error occurred executing an script condition, details = ${JSON.stringify(err, null, 2).substring(
+          0,
+          300
+        )}`
+      );
+    } finally {
+      isolate.cleanup();
+    }
   }
 }
