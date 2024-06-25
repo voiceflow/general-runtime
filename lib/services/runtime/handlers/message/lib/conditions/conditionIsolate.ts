440703caeb100d165cdc42f63d08df19ea6de626
Brennan Lu <brennan.lu314@gmail.com>
2024-06-25 10:21:26 -0400

    feat: added script condition

--- lib/services/runtime/handlers/message/lib/conditions/conditionIsolate.ts
+++ lib/services/runtime/handlers/message/lib/conditions/conditionIsolate.ts
@@ -5,12 +5,65 @@ export class ConditionIsolate {
 
   private context?: ivm.Context;
 
+  private static readonly userFunctionName = `userFunction`;
+
+  private static readonly userModuleName = `userModule`;
+
+  private static readonly userVariablesName = `inputVars`;
+
+  private static readonly resolveFuncName = `__resolve__`;
+
+  private static readonly rejectFuncName = `__reject__`;
+
   constructor(private readonly variables: Record<string, unknown>) {}
 
   private readonly ISOLATED_VM_LIMITS = {
     maxMemoryMB: 8,
     maxExecutionTimeMs: 1 * 1000,
   };
+
+  private async setupResolve(resolve: (value: any) => void, reject: (reason?: any) => void) {
+    await this.context!.evalClosure(
+      `
+            ${ConditionIsolate.resolveFuncName} = function(...args) {
+                return $0.apply(undefined, args, { arguments: { copy: true }});
+            }
+            ${ConditionIsolate.rejectFuncName} = function(...args) {
+                return $1.apply(undefined, args, { arguments: { copy: true }});
+            }
+        `,
+      [resolve, reject],
+      { arguments: { reference: true } }
+    );
+  }
+
+  private async setupUserVariables() {
+    await this.context!.global.set(ConditionIsolate.userVariablesName, this.variables, { copy: true });
+  }
+
+  private async compileUserModule(code: string) {
+    return this.isolate!.compileModule(code);
+  }
+
+  private async compileMainModule() {
+    const sanitizeVarsFromGlobal = `
+            const ${ConditionIsolate.userVariablesName} = globalThis.${ConditionIsolate.userVariablesName};
+            delete globalThis.${ConditionIsolate.userVariablesName};
+        `;
+
+    return this.isolate!.compileModule(`
+        import ${ConditionIsolate.userFunctionName} from '${ConditionIsolate.userModuleName}';
+        ${sanitizeVarsFromGlobal}
+        (function () {
+          try { 
+            const result = ${ConditionIsolate.userFunctionName}({ ${ConditionIsolate.userVariablesName} });
+            ${ConditionIsolate.resolveFuncName}(result);
+          } catch (err) {
+            ${ConditionIsolate.rejectFuncName}(err);
+          }
+        })();
+      `);
+  }
 
   public async initialize(): Promise<void> {
     this.isolate = new ivm.Isolate({
@@ -27,6 +80,47 @@ export class ConditionIsolate {
   public async cleanup(): Promise<void> {
     this.context?.release();
     this.isolate?.dispose();
+  }
+
+  public async executeUserModule(code: string) {
+    if (!this.isolate || !this.context) {
+      throw new Error(`condition isolate was not initialized before an attempt to execute code`);
+    }
+
+    const userModule = await this.compileUserModule(code);
+    const mainModule = await this.compileMainModule();
+    await this.setupUserVariables();
+
+    const executeCode = async (resolve: (val: unknown) => void, reject: (reason?: unknown) => void) => {
+      try {
+        await this.setupResolve(resolve, reject);
+
+        await userModule.instantiate(this.context!, () => {
+          throw new Error(`User code cannot import modules.`);
+        });
+
+        await mainModule.instantiate(this.context!, (specifier: string) => {
+          if (specifier === ConditionIsolate.userModuleName) {
+            return userModule;
+          }
+          throw new Error(`Module '${specifier}' does not exist.`);
+        });
+
+        await mainModule.evaluate({
+          timeout: this.ISOLATED_VM_LIMITS.maxExecutionTimeMs,
+        });
+      } catch (err) {
+        reject(err);
+      }
+    };
+
+    return new Promise((resolve, reject) => {
+      try {
+        executeCode(resolve, reject);
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
 
   public async executeCode(code: string): Promise<unknown> {
