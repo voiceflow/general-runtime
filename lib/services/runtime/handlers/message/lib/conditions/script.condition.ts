7b3d74ab566fd9d6bbdfb770a4b37e1437b7af0d
Zhi Heng (Brennan) Lu <brennan.lu314@gmail.com>
2024-06-28 20:37:10 +0000

    feat: add script condition (PL-1045)  (#853)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements PL-1045**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    Adds the Script Condition for the Message Step, which allows users to execute JavaScript code to evaluate a condition.

--- lib/services/runtime/handlers/message/lib/conditions/script.condition.ts
+++ lib/services/runtime/handlers/message/lib/conditions/script.condition.ts
@@ -1,13 +1,37 @@
 import { CompiledScriptCondition } from '@voiceflow/dtos';
 
 import { BaseCondition } from './base.condition';
+import { ConditionIsolate } from './conditionIsolate';
 
 export class ScriptCondition extends BaseCondition<CompiledScriptCondition> {
-  private evaluateCode(_code: string): boolean {
-    throw new Error('script condition is not implemented');
+  private async evaluateCode(isolate: ConditionIsolate, code: string): Promise<boolean> {
+    const result = await isolate.executeUserModule(code);
+    return !!result;
   }
 
-  public evaluate(): boolean {
-    return this.evaluateCode(this.condition.data.code);
+  public async evaluate(): Promise<boolean> {
+    this.log('--- evaluating script ---');
+
+    const isolate = new ConditionIsolate(this.variables);
+    try {
+      await isolate.initialize();
+      const result = await this.evaluateCode(isolate, this.condition.data.code);
+
+      this.log(`--- script evaluated to ${result} ---`);
+
+      return result;
+    } catch (err) {
+      if (err instanceof Error) {
+        throw new Error(`an error occurred executing a script condition, msg = ${err.message}`);
+      }
+      throw new Error(
+        `an unknown error occurred executing a script condition, details = ${JSON.stringify(err, null, 2).substring(
+          0,
+          300
+        )}`
+      );
+    } finally {
+      isolate.cleanup();
+    }
   }
 }
---
+++ tests/lib/services/runtime/handlers/message/lib/conditions/script.condition.unit.ts
@@ -0,0 +1,54 @@
+import { ConditionType } from '@voiceflow/dtos';
+import { expect } from 'chai';
+
+import { ScriptCondition } from '@/lib/services/runtime/handlers/message/lib/conditions/script.condition';
+
+describe('ScriptCondition', () => {
+  let variables: Record<string, unknown>;
+
+  before(() => {
+    variables = {
+      propA: 1,
+      propB: 'hello',
+    };
+  });
+
+  describe('evaluate', () => {
+    it('works', async () => {
+      const condition = new ScriptCondition(
+        {
+          type: ConditionType.SCRIPT,
+          data: {
+            code: `export default function main({ variables }) { 
+            const { propA, propB } = variables;
+            return propA + propB === "1hello";
+          }`,
+          },
+        },
+        variables
+      );
+
+      const result = await condition.evaluate();
+
+      expect(result).to.eql(true);
+    });
+
+    it('always produces a boolean', async () => {
+      const condition = new ScriptCondition(
+        {
+          type: ConditionType.SCRIPT,
+          data: {
+            code: `export default function main() { 
+            return "this is not a boolean";
+          }`,
+          },
+        },
+        variables
+      );
+
+      const result = await condition.evaluate();
+
+      expect(result).to.eql(true);
+    });
+  });
+});
