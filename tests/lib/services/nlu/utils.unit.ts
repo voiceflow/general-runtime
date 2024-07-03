cec16ff3a1121e0442df4acfae8bc7df669f229b
Tyler Stewart <git@tylerstewart.ca>
2024-07-03 15:22:04 -0600

    feat: bypass nlu based on current node

--- tests/lib/services/nlu/utils.unit.ts
+++ tests/lib/services/nlu/utils.unit.ts
@@ -1,13 +1,9 @@
 import { AlexaConstants } from '@voiceflow/alexa-types';
-import { BaseNode } from '@voiceflow/base-types';
-import { CommandType, EventType } from '@voiceflow/base-types/build/cjs/node/utils';
 import { VoiceflowConstants } from '@voiceflow/voiceflow-types';
 import chai from 'chai';
 import sinon from 'sinon';
 
-import { getAvailableIntentsAndEntities, mapChannelData } from '@/lib/services/nlu/utils';
-
-import { getMockRuntime } from './fixture';
+import { mapChannelData } from '@/lib/services/nlu/utils';
 
 const { expect } = chai;
 
@@ -40,103 +36,6 @@ describe('nlu manager utils unit tests', () => {
 
       const expectData = { payload: { intent: { name: AlexaConstants.AmazonIntent.YES } } };
       expect(outputData).to.eql(expectData);
-    });
-  });
-
-  describe('getAvailableIntentsAndEntities', () => {
-    const mockContext = { data: { api: sinon.stub() } };
-    const mockPizzaIntent = { type: EventType.INTENT, intent: 'Pizza', mappings: [{ slot: 'foo' }, { slot: 'bar' }] };
-    const mockYesIntent = { type: EventType.INTENT, intent: 'VF.YES', mappings: [{ slot: 'baz' }] };
-    const mockNoIntent = { type: EventType.INTENT, intent: 'VF.NO', mappings: [{ slot: 'qux' }] };
-
-    it('takes intersection when node is scoped', async () => {
-      const mockNodeInteractions = [{ event: mockPizzaIntent }, { event: mockYesIntent }, { event: mockNoIntent }];
-
-      const mockNode = {
-        interactions: mockNodeInteractions,
-        intentScope: BaseNode.Utils.IntentScope.NODE,
-      };
-
-      const mockCommands = [
-        {
-          type: CommandType.JUMP,
-          event: mockYesIntent,
-        },
-        {
-          type: CommandType.JUMP,
-          event: mockPizzaIntent,
-        },
-      ];
-
-      const runtimeClient = {
-        createRuntime: sinon.stub().returns(getMockRuntime(mockCommands, mockNode)),
-      };
-
-      const mockRuntimeManager = {
-        createClient: sinon.stub().returns(runtimeClient),
-      };
-
-      const { availableIntents, availableEntities } = await getAvailableIntentsAndEntities(
-        mockRuntimeManager as any,
-        mockContext as any
-      );
-
-      expect(availableIntents).to.eql(new Set(['VF.YES', 'Pizza']));
-      expect(availableEntities).to.eql(new Set(['baz', 'foo', 'bar']));
-    });
-
-    it('ignores node level when intentScope is global', async () => {
-      const mockNodeInteractions = [
-        {
-          event: mockYesIntent,
-        },
-      ];
-
-      const mockNode = {
-        interactions: mockNodeInteractions,
-        intentScope: BaseNode.Utils.IntentScope.GLOBAL,
-      };
-
-      const mockCommands = [
-        {
-          type: CommandType.JUMP,
-          event: mockNoIntent,
-        },
-      ];
-
-      const runtimeClient = {
-        createRuntime: sinon.stub().returns(getMockRuntime(mockCommands, mockNode)),
-      };
-
-      const mockRuntimeManager = {
-        createClient: sinon.stub().returns(runtimeClient),
-      };
-
-      const { availableIntents, availableEntities } = await getAvailableIntentsAndEntities(
-        mockRuntimeManager as any,
-        mockContext as any
-      );
-
-      expect(availableIntents).to.eql(new Set(['VF.NO']));
-      expect(availableEntities).to.eql(new Set(['qux']));
-    });
-
-    it('works with empty params', async () => {
-      const runtimeClient = {
-        createRuntime: sinon.stub().returns(getMockRuntime()),
-      };
-
-      const mockRuntimeManager = {
-        createClient: sinon.stub().returns(runtimeClient),
-      };
-
-      const { availableIntents, availableEntities } = await getAvailableIntentsAndEntities(
-        mockRuntimeManager as any,
-        mockContext as any
-      );
-
-      expect(availableIntents).to.eql(new Set([]));
-      expect(availableEntities).to.eql(new Set([]));
     });
   });
 });
