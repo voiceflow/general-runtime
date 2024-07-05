dbfec5b8e9504d8f60eef635107f71e661d75070
Tyler <tylerhan97@gmail.com>
2024-07-05 15:57:41 -0400

    fix: remove prompt condition (ENG-000)

--- lib/services/runtime/handlers/message/lib/conditions/condition.ts
+++ lib/services/runtime/handlers/message/lib/conditions/condition.ts
@@ -2,7 +2,6 @@ import { AnyCompiledCondition, ConditionType } from '@voiceflow/dtos';
 
 import { BaseCondition, BaseConditionLogger } from './base.condition';
 import { ExpressionCondition } from './expression.condition';
-import { PromptCondition } from './prompt.condition';
 import { ScriptCondition } from './script.condition';
 
 export function createCondition(
@@ -15,8 +14,6 @@ export function createCondition(
       return new ExpressionCondition(condition, variables, log);
     case ConditionType.SCRIPT:
       return new ScriptCondition(condition, variables, log);
-    case ConditionType.PROMPT:
-      return new PromptCondition(condition, variables, log);
     default:
       throw new Error(`received unexpected condition type`);
   }
