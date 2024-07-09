a0ed6a0d8873e57a6b2e64b3a68fafbc8e729256
Brennan Lu <brennan.lu314@gmail.com>
2024-07-09 16:16:18 -0400

    fix: fixing crash due to empty variants

--- lib/services/runtime/handlers/message/message.handler.ts
+++ lib/services/runtime/handlers/message/message.handler.ts
@@ -74,7 +74,9 @@ export const MessageHandler: HandlerFactory<CompiledMessageNode> = () => ({
 
       const chosenVariant = await selectVariant(preprocessedVariants);
 
-      outputVariant(chosenVariant, node, runtime, variables);
+      if (chosenVariant.data.text.trim()) {
+        outputVariant(chosenVariant, node, runtime, variables);
+      }
 
       return node.ports.default;
     } catch (err) {
