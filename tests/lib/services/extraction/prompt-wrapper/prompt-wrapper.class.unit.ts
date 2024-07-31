5024251a39d5734b3723dad3fb4ddac430faa348
Greg O'Grady <gregory.ogrady@voiceflow.com>
2024-07-31 10:55:47 -0400

    test: add PromptWrapper tests

---
+++ tests/lib/services/extraction/prompt-wrapper/prompt-wrapper.class.unit.ts
@@ -0,0 +1,50 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+
+import { PromptWrapper } from '@/lib/services/extraction/prompt-wrapper/prompt-wrapper.class';
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+const GENERAL_SERVICE_ENDPOINT = 'http://localhost:6970';
+const defaultConfig = {
+  GENERAL_SERVICE_ENDPOINT,
+  CLOUD_ENV: '',
+  NLU_GATEWAY_SERVICE_URI: '',
+  NLU_GATEWAY_SERVICE_PORT_APP: '',
+};
+
+describe('prompt wrapper unit tests', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  const setup = () => ({
+    config: {
+      ...defaultConfig,
+      mlGateway: {
+        private: {
+          completion: {
+            generateCompletion: sinon.stub().resolves({ test: 'beans' }),
+          },
+        },
+      },
+    },
+  });
+
+  it('works with openSlot false', async () => {
+    const utterance = 'query-val';
+    const { config } = setup();
+
+    const expectedResult = {};
+    const expectedSideEffects = {};
+
+    const promptWrapper = new PromptWrapper(config).withUtterance(utterance);
+
+    const [result, sideEffects] = await promptWrapper.exec();
+
+    expect(result).to.eql(expectedResult);
+    expect(sideEffects).to.eql(expectedSideEffects);
+  });
+});
