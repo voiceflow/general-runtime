a874f16847d00322b20b86d9f5095e079b5dadc5
Peter Jiang <2892084432j@gmail.com>
2024-07-31 17:11:04 -0400

    feat: add bad node id test

--- tests/runtime/lib/Handlers/code/index.unit.ts
+++ tests/runtime/lib/Handlers/code/index.unit.ts
@@ -97,5 +97,23 @@ describe('CodeHandler', () => {
       expect(store.get('a')).to.eq(0);
       mockLog.verify();
     });
+
+    it('bad node id', async () => {
+      const variables = { a: 0 };
+      const store = new Store(variables);
+
+      sandbox.mock(log).expects('warn').once().calledOnceWith('unable to parse node id: garbage');
+
+      const mockNode = createMock<BaseNode.Code.Node>({
+        // dated 2024-07-25
+        id: 'garbage',
+        success_id: 'success-id',
+        fail_id: 'fail-id',
+        code: `a = 5`,
+      });
+
+      await handler.handle(mockNode, mockRuntime, store, mockProgram);
+      mockLog.verify();
+    });
   });
 });
